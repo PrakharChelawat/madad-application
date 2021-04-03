0cad3097b0f5a921103831683b2c0069578ed838
Prakhar Chelawat <44531154+PrakharChelawat@users.noreply.github.com>
2021-04-03 23:13:44 +0530

    Add files via upload

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports={
+    // MONGOURI:"mongodb+srv://Prakhar:ZGoiL0jE7bH8ZvqA@cluster0.o3ghm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://rakhar:YvSLQsRPM8If0119@cluster0.o3ghm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"qsefthilpokuhyfrd",
+    SENDGRID_API:"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",
+    EMAIL:"http:localhost:3000",
+}
