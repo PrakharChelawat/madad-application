a83daaa15c211f1a5de654e124a3bb0ebb780be3
Prakhar Chelawat <44531154+PrakharChelawat@users.noreply.github.com>
2021-04-03 23:13:01 +0530

    Delete keys.js

--- server/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    // MONGOURI:"mongodb+srv://Prakhar:ZGoiL0jE7bH8ZvqA@cluster0.o3ghm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    MONGOURI:"mongodb+srv://rakhar:YvSLQsRPM8If0119@cluster0.o3ghm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"qsefthilpokuhyfrd"
-}
